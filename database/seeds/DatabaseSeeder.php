b6fd6e86f922277e4b72bc12118088e99a9be130
Ajay Makwana <ajay.makwana@levaral.com>
2017-09-18 16:30:00 +0530

    Add Payment seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(AddressesTableSeeder::class);
         $this->call(ExchangeRatesTableSeeder::class);
+        $this->call(PaymentsTableSeeder::class);
     }
 }
