cfa34ca51c9cd41bc90ed578d89f45caa9323951
Ajay Makwana <ajay.makwana@levaral.com>
2017-09-18 13:29:53 +0530

    Add ExchangeRate seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(AddressesTableSeeder::class);
+        $this->call(ExchangeRatesTableSeeder::class);
     }
 }
---
+++ database/seeds/ExchangeRatesTableSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Domain\ExchangeRate\ExchangeRate;
+use Illuminate\Database\Seeder;
+
+class ExchangeRatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = \Carbon\Carbon::now();
+        $faker = \Faker\Factory::create();
+        $currencies = [
+            [
+                'currency' => 'bitcoin',
+                'dollar' => 3855.250,
+                'min' => -500,
+                'max' => 500,
+            ],
+            [
+                'currency' => 'litecoin',
+                'dollar' => 285.919,
+                'min' => -50,
+                'max' => 50,
+            ],
+            [
+                'currency' => 'ethereum',
+                'dollar' => 53.0299,
+                'min' => -5,
+                'max' => 5,
+            ],
+        ];
+
+        $exchangeRates = [];
+        $daysAgo = $now->diffInDays($now->copy()->subMonth());
+
+        for ($day = $daysAgo; $day >= 0; $day -= 1) {
+            $date = $now->copy()->subDay($day);
+            
+            foreach ($currencies as $currency) {
+                $dollar = $faker->numberBetween($currency['min'], $currency['max']) + $currency['dollar'];
+
+                $exchangeRates[] = [
+                    'currency' => $currency['currency'],
+                    'amount' => $dollar * config('app.exchangeRate.' . $currency['currency']),
+                    'dollar' => $dollar,
+                    'start_date' => $date->toDateTimeString(),
+                    'end_date' => $day == 0 ? null : $date->copy()->addDay(),
+                    'created_at' => $date->toDateTimeString(),
+                    'updated_at' => $date->toDateTimeString(),
+                ];
+            }
+        }
+
+        ExchangeRate::insert($exchangeRates);
+    }
+}
