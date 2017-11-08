c381ab89dcea8408d584696fe70bb7a05d7a539c
Ajay Makwana <ajay.makwana@levaral.com>
2017-11-08 12:53:02 +0530

    Change favicon icon

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -7,7 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Icons -->
-    <link rel="shortcut icon" href="/images/favicon.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="manifest" href="images/manifest.json">
+    <link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
